a7ea6a52f01ab2051d1f935b9bee0a66b8e64865
TymekBrunka <tymkopolo@wp.pl>
2024-11-20 11:35:40 +0100

    lualine

--- Session.vim
+++ Session.vim
@@ -3,7 +3,7 @@ let s:so_save = &g:so | let s:siso_save = &g:siso | setg so=0 siso=0 | setl so=-
 let v:this_session=expand("<sfile>:p")
 silent only
 silent tabonly
-cd ~/.config/nvim
+cd ~/AppData/Local/nvim
 if expand('%') == '' && !&modified && line('$') <= 1 && getline(1) == ''
   let s:wipebuf = bufnr('%')
 endif
@@ -13,18 +13,10 @@ if &shortmess =~ 'A'
 else
   set shortmess=aoO
 endif
-badd +49 after/plugin/nxwm.lua
-badd +214 after/plugin/lualine.lua
-badd +21 lua/mappings.lua
-badd +193 lua/plugins-list.lua
-badd +141 after/plugin/cmake-tools.lua
-badd +1 ~/.config/nvim/init.lua
-badd +38 ~/.config/nvim/lua/tab-keybinds.lua
 argglobal
 %argdel
-edit after/plugin/lualine.lua
 argglobal
-balt ~/.config/nvim/lua/tab-keybinds.lua
+enew
 setlocal fdm=manual
 setlocal fde=0
 setlocal fmr={{{,}}}
@@ -33,14 +25,6 @@ setlocal fdl=99
 setlocal fml=1
 setlocal fdn=20
 setlocal fen
-silent! normal! zE
-let &fdl = &fdl
-let s:l = 214 - ((19 * winheight(0) + 13) / 26)
-if s:l < 1 | let s:l = 1 | endif
-keepjumps exe s:l
-normal! zt
-keepjumps 214
-normal! 05|
 tabnext 1
 if exists('s:wipebuf') && len(win_findbuf(s:wipebuf)) == 0 && getbufvar(s:wipebuf, '&buftype') isnot# 'terminal'
   silent exe 'bwipe ' . s:wipebuf
--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -224,25 +224,30 @@ ins_left {
     color = { fg = colors.violet, gui = 'bold' },
 }
 
+-- ins_left {
+--     -- Lsp server name .
+--     function()
+--         local msg = 'No Active Lsp'
+--         local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+--         local clients = vim.lsp.get_active_clients()
+--         if next(clients) == nil then
+--             return msg
+--         end
+--         for _, client in ipairs(clients) do
+--             local filetypes = client.config.filetypes
+--             if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+--                 return client.name
+--             end
+--         end
+--         return msg
+--     end,
+--     icon = ' LSP:',
+--     color = { fg = '#aaaaaa', gui = 'bold' },
+-- }
+
 ins_left {
-    -- Lsp server name .
-    function()
-        local msg = 'No Active Lsp'
-        local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-        local clients = vim.lsp.get_active_clients()
-        if next(clients) == nil then
-            return msg
-        end
-        for _, client in ipairs(clients) do
-            local filetypes = client.config.filetypes
-            if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                return client.name
-            end
-        end
-        return msg
-    end,
-    icon = ' LSP:',
-    color = { fg = '#aaaaaa', gui = 'bold' },
+    'buffers',
+    mode=2,
 }
 
 -- Add components to right sections
